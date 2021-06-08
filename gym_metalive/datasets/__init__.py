7dc230c9820ca23cc1f78c9dd94ddaabe91c8719
dhupee <32934122+dhupee@users.noreply.github.com>
2021-06-08 17:31:10 +0700

    deleting some

--- gym_metalive/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils import load_dataset as _load_dataset
-
-
-# Load FOREX datasets
-FOREX_EURUSD_1H_ASK = _load_dataset('FOREX_EURUSD_1H_ASK', 'Time')
--- gym_metalive/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from .trading_env import TradingEnv, Actions, Positions
-from .forex_env import ForexEnv
-from .forex_env_copy import ForexEnv
