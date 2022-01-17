d25428cea77be7968f5ef2cafefacfc18e4207c8
dhupee <32934122+dhupee@users.noreply.github.com>
2022-01-18 01:46:14 +0700

    gonna stop working on this for now, branching

--- MetaNgepet-Live.py
+++ MetaNgepet-Live.py
@@ -16,7 +16,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-import pickle
+import joblib
 
 #===================================================================
 
@@ -36,15 +36,15 @@ bars = 500
 trading_day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 trading_minute = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]
 
-model_filename = 'D:/DH/projects/MetaNgepet/used_model-scaler/EURUSD_5/MetaNgepet_EURUSD_5_LinearReg_Model.pkl'
-scaler_filename = 'D:/DH/projects/MetaNgepet/used_model-scaler/EURUSD_5/MetaNgepet_EURUSD_5_LinearReg_Scaler.pkl'
+model_filename = 'used_model/Model_MetaNgepet_{}_{}.pkl'.format(symbol, timeframe)
+scaler_filename = 'used_model/Scaler_MetaNgepet_{}_{}.pkl'.format(symbol, timeframe)
 
 trade_check = False
 
 #===================================================================
 
-model = pickle.load(open(model_filename, 'rb'))
-scaler = pickle.load(open(scaler_filename, 'rb'))
+model = joblib.load(open(model_filename, 'rb'))
+scaler = joblib.load(open(scaler_filename, 'rb'))
 
 mt5.initialize(
    path = path,          # path to the MetaTrader 5 terminal EXE file
@@ -123,145 +123,101 @@ def generate_features(df):
         df (dataframe with columns "open", "close", "high", "low", "volume")
     Returns:
         dataframe, data set with new features
-    Timeframe for calculation:
-        6 timeframe for 30 minutes
-        48 timeframe for 4 hours
-        144 timeframe for 12 hours
-        288 timeframe for 1 day
+    Timeframe for calculation (5 minutes timeframe):
+        6 bars for 30 minutes
+        48 bars for 4 hours
+        144 bars for 12 hours
+        288 bars for 1 day
     """
     df_new = pd.DataFrame()
     
-    # 6 original features
+    # Real time features
     df_new['open'] = df['open']
-    df_new['open_5m'] = df['open'].shift(1)
-    df_new['close_5m'] = df['close'].shift(1)
-    df_new['high_5m'] = df['high'].shift(1)
-    df_new['low_5m'] = df['low'].shift(1)
-    df_new['volume_5m'] = df['volume'].shift(1)
-    df_new['spread_5m'] = df['spread'].shift(1)
+
+    # original features
+    df_new['open_1'] = df['open'].shift(1)
+    df_new['close_1'] = df['close'].shift(1)
+    df_new['high_1'] = df['high'].shift(1)
+    df_new['low_1'] = df['low'].shift(1)
+    df_new['volume_1'] = df['volume'].shift(1)
     
-    # 50 original features
-    # average price
-    df_new['avg_price_30m'] = df['close'].rolling(window=6).mean().shift(1)
-    df_new['avg_price_4h'] = df['close'].rolling(window=48).mean().shift(1)
-    df_new['avg_price_12h'] = df['close'].rolling(window=144).mean().shift(1)
-    df_new['avg_price_1d'] = df['close'].rolling(window=288).mean().shift(1)
-    
+    # indicator
+    RSI = TA.RSI(df, 6).shift(1)
+    AO = TA.AO(df, 34, 5).shift(1)
+
+    df_new['EMA_30m_1'] = TA.EMA(df, 6).shift(1)
+    df_new['EMA_4h_1'] = TA.EMA(df, 48).shift(1)
+    df_new['RSI_1'] = RSI.shift(1)
+    df_new['AO_1'] = AO.shift(1)
+
+    # price growth
+    df_new['price_growth_1-2'] = df['close'].shift(1) - df['close'].shift(2)
+    df_new['price_growth_2-3'] = df['close'].shift(2) - df['close'].shift(3)
+    df_new['price_growth_3-4'] = df['close'].shift(3) - df['close'].shift(4)
+    df_new['price_growth_4-5'] = df['close'].shift(4) - df['close'].shift(5)
+
+    # RSI growth
+    df_new['RSI_growth_1-2'] = RSI.shift(1) - RSI.shift(2)
+    df_new['RSI_growth_2-3'] = RSI.shift(2) - RSI.shift(3)
+    df_new['RSI_growth_1-2'] = RSI.shift(1) - RSI.shift(2)
+    df_new['RSI_growth_1-2'] = RSI.shift(1) - RSI.shift(2)
+
+    # AO growth
+    df_new['AO_growth_1-2'] = AO.shift(1) - AO.shift(2)
+    df_new['AO_growth_2-3'] = AO.shift(2) - AO.shift(3)
+    df_new['AO_growth_3-4'] = AO.shift(3) - AO.shift(4)
+    df_new['AO_growth_3-4'] = AO.shift(4) - AO.shift(5)
+
     # average price ratio
-    df_new['ratio_avg_price_30m_4h'] = df_new['avg_price_30m'] / df_new['avg_price_4h']
-    df_new['ratio_avg_price_30m_12h'] = df_new['avg_price_30m'] / df_new['avg_price_12h']
-    df_new['ratio_avg_price_30m_1d'] = df_new['avg_price_30m'] / df_new['avg_price_1d']
-    df_new['ratio_avg_price_4h_12h'] = df_new['avg_price_4h'] / df_new['avg_price_12h']
-    df_new['ratio_avg_price_4h_1d'] = df_new['avg_price_4h'] / df_new['avg_price_1d']
-    df_new['ratio_avg_price_12h_1d'] = df_new['avg_price_12h'] / df_new['avg_price_1d']                                            
-    
-    
+    df_new['ratio_avg_price_30m_4h'] = df_new['EMA_30m_1'] / df_new['EMA_4h_1']
+   
     # average volume
     df_new['avg_volume_30m'] = df['volume'].rolling(window=6).mean().shift(1)
     df_new['avg_volume_4h'] = df['volume'].rolling(window=48).mean().shift(1)
-    df_new['avg_volume_12h'] = df['volume'].rolling(window=144).mean().shift(1)
-    df_new['avg_volume_1d'] = df['volume'].rolling(window=288).mean().shift(1)
     
     # average volume ratio
     df_new['ratio_avg_volume_30m_4h'] = df_new['avg_volume_30m'] / df_new['avg_volume_4h']
-    df_new['ratio_avg_volumee_30m_12h'] = df_new['avg_volume_30m'] / df_new['avg_volume_12h']                                                   
-    df_new['ratio_avg_volume_30m_1d'] = df_new['avg_volume_30m'] / df_new['avg_volume_1d']
-    df_new['ratio_avg_volume_4h_12h'] = df_new['avg_volume_4h'] / df_new['avg_volume_12h']
-    df_new['ratio_avg_volume_4h_1d'] = df_new['avg_volume_4h'] / df_new['avg_volume_1d']
-    df_new['ratio_avg_volume_12h_1d'] = df_new['avg_volume_12h'] / df_new['avg_volume_1d']                                                 
-    
     
     # standard deviation of prices
     df_new['std_price_30m'] = df['close'].rolling(window=6).std().shift(1)
     df_new['std_price_4h'] = df['close'].rolling(window=48).std().shift(1)
-    df_new['std_price_12h'] = df['close'].rolling(window=144).std().shift(1)                                               
-    df_new['std_price_1d'] = df['close'].rolling(window=288).std().shift(1)
     
     # standard deviation ratio of prices 
     df_new['ratio_std_price_30m_4h'] = df_new['std_price_30m'] / df_new['std_price_4h']
-    df_new['ratio_std_price_30m_12h'] = df_new['std_price_30m'] / df_new['std_price_12h']
-    df_new['ratio_std_price_30m_1d'] = df_new['std_price_30m'] / df_new['std_price_1d']
-    df_new['ratio_std_price_4h_12h'] = df_new['std_price_4h'] / df_new['std_price_12h'] 
-    df_new['ratio_std_price_3h_1d'] = df_new['std_price_4h'] / df_new['std_price_1d']                                               
-    df_new['ratio_std_price_12h_1d'] = df_new['std_price_12h'] / df_new['std_price_1d']                                                
-    
     
     # standard deviation of volumes
     df_new['std_volume_30m'] = df['volume'].rolling(window=6).std().shift(1)
     df_new['std_volume_4h'] = df['volume'].rolling(window=48).std().shift(1)
-    df_new['std_volume_12h'] = df['volume'].rolling(window=144).std().shift(1)
-    df_new['std_volume_1d'] = df['volume'].rolling(window=288).std().shift(1)
     
     # standard deviation ratio of volumes
-    df_new['ratio_std_volume_30m_4h'] = df_new['std_volume_30m'] / df_new['std_volume_4h']
-    df_new['ratio_std_volume_30m_12h'] = df_new['std_volume_30m'] / df_new['std_volume_12h']
-    df_new['ratio_std_volume_30m_1d'] = df_new['std_volume_30m'] / df_new['std_volume_1d']
-    df_new['ratio_std_volume_4h_12h'] = df_new['std_volume_4h'] / df_new['std_volume_12h'] 
-    df_new['ratio_std_volume_3h_1d'] = df_new['std_volume_4h'] / df_new['std_volume_1d']                                               
-    df_new['ratio_std_volume_12h_1d'] = df_new['std_volume_12h'] / df_new['std_volume_1d']                                               
-                                                   
-    # return
-    df_new['return_5m'] = ((df['close'] - df['close'].shift(1)) / df['close'].shift(1)).shift(1)
-    df_new['return_30m'] = ((df['close'] - df['close'].shift(6)) / df['close'].shift(6)).shift(1)
-    df_new['return_4h'] = ((df['close'] - df['close'].shift(48)) / df['close'].shift(48)).shift(1)
-    df_new['return_12h'] = ((df['close'] - df['close'].shift(144)) / df['close'].shift(144)).shift(1)                                                
-    df_new['return_1d'] = ((df['close'] - df['close'].shift(288)) / df['close'].shift(288)).shift(1)
-    
-    # average of return
-    df_new['return_avg_30m'] = df_new['return_5m'].rolling(window=6).mean()
-    df_new['return_avg_4h'] = df_new['return_5m'].rolling(window=48).mean()
-    df_new['return_avg_12h'] = df_new['return_5m'].rolling(window=144).mean()
-    df_new['return_avg_1d'] = df_new['return_5m'].rolling(window=288).mean()
-
-    # indicator
-    df_new['EMA_4h'] = TA.EMA(df, 48)
+    df_new['ratio_std_volume_30m_4h'] = df_new['std_volume_30m'] / df_new['std_volume_4h']                                                                                  
 
     # the target
-    # df_new['close'] = df['close']
+    #df_new['close'] = df['close']
     df_new = df_new.dropna(axis=0)
     return df_new
 
 def generate_signal(df_features, rates_frame, predictions):
     df_signal = pd.DataFrame(index=df_features.index)
-    
+
+    df_signal['open_1'] = rates_frame['open'].shift(1)
+    df_signal['close_1'] = rates_frame['close'].shift(1)
+    df_signal['high_1'] = rates_frame['high'].shift(1)
+    df_signal['low_1'] = rates_frame['low'].shift(1)
+
     df_signal['predictions'] = predictions
+
+
     df_signal['predictions_5m'] = df_signal['predictions'].shift(1)
     df_signal['EMA_30m'] = TA.EMA(rates_frame, 6)
     df_signal['EMA_4h'] = TA.EMA(rates_frame, 48)
-    df_signal['SAR'] = TA.SAR(rates_frame, 0.02, 0.2)
-    df_signal['CCI_30m'] = TA.CCI(rates_frame, 6)
 
-    #df_signal['avg_price_30m'] = rates_frame['close'].rolling(window=6).mean().shift(1)
-    #df_signal['avg_price_4h'] = rates_frame['close'].rolling(window=48).mean().shift(1)
-
-    df_signal['open'] = rates_frame['open']
-    df_signal['open_5m'] = rates_frame['open'].shift(1)
-    df_signal['close_5m'] = rates_frame['close'].shift(1)
-    df_signal['high_5m'] = rates_frame['high'].shift(1)
-    df_signal['low_5m'] = rates_frame['low'].shift(1)
-
-    df_signal['lowest_30m'] = rates_frame['low'].rolling(window=6).min().shift(1)
-    df_signal['highest_30m'] = rates_frame['high'].rolling(window=6).max().shift(1)
-
-    return df_signal
 
 def get_buy_condition(df_signal, now_datetime, digits):
+    pass
+
     # get signal values
     prediction_value = round(df_signal.loc[now_datetime]["predictions"], digits)
-    prediction_5m_value = round(df_signal.loc[now_datetime]["predictions_5m"], digits)
-    
-    ema_30m = round(df_signal.loc[now_datetime]["EMA_30m"], digits)
-    ema_4h = round(df_signal.loc[now_datetime]["EMA_4h"], digits)
-    sar = df_signal.loc[now_datetime]["SAR"]
-    cci_30m = df_signal.loc[now_datetime]["CCI_30m"]
-
-    lowest_30m = df_signal.loc[now_datetime]["lowest_30m"]
-
-    open = round(df_signal.loc[now_datetime]["open"], digits)
-    open_5m = round(df_signal.loc[now_datetime]["open_5m"], digits)
-    close_5m = round(df_signal.loc[now_datetime]["close_5m"], digits)
-    high_5m = round(df_signal.loc[now_datetime]["high_5m"], digits)
-    #low_5m = round(df_signal.loc[now_datetime]["low_5m"], digits)
 
     if open > ema_4h and open > sar and ema_30m > ema_4h and open > lowest_30m and close_5m > open_5m and prediction_value > open and cci_30m > 100 :
         buy_condition = True
@@ -271,6 +227,8 @@ def get_buy_condition(df_signal, now_datetime, digits):
     return buy_condition
 
 def get_sell_condition(df_signal, now_datetime, digits):
+    pass
+
     # get signal values
     prediction_value = round(df_signal.loc[now_datetime]["predictions"], digits)
     prediction_5m_value = round(df_signal.loc[now_datetime]["predictions_5m"], digits)
@@ -366,42 +324,20 @@ while True:
                 #print(df_signal)
 
                 prediction_value = round(df_signal.loc[now_datetime]["predictions"], digits)
-                
 
-                prediction_5m_value = round(df_signal.loc[now_datetime]["predictions_5m"], digits)
-                ema_30m = round(df_signal.loc[now_datetime]["EMA_30m"], digits)
-                ema_4h = round(df_signal.loc[now_datetime]["EMA_4h"], digits)
-                sar = df_signal.loc[now_datetime]["SAR"]
-                cci_30m = df_signal.loc[now_datetime]["CCI_30m"]
-                highest_30m = df_signal.loc[now_datetime]["highest_30m"]
-                lowest_30m = df_signal.loc[now_datetime]["lowest_30m"]
-                open = round(df_signal.loc[now_datetime]["open"], digits)
-                open_5m = round(df_signal.loc[now_datetime]["open_5m"], digits)
-                close_5m = round(df_signal.loc[now_datetime]["close_5m"], digits)
-                high_5m = round(df_signal.loc[now_datetime]["high_5m"], digits)
-                low_5m = round(df_signal.loc[now_datetime]["low_5m"], digits)
-
-                print('prediction value =',prediction_value)
-                print('open = ', open)
-                print('close_5m', close_5m)
-                print('open_5m', open_5m)
-                print('ema 30m =', ema_30m)
-                print('ema_4h =', ema_4h)
-                print('sar =', sar)
-                print('cci_30m =', cci_30m)
-                print('highest_30m =', highest_30m)
-                print('lowest_30m =', lowest_30m)
-
-
+                """
                 buy_condition = get_buy_condition(df_signal, now_datetime, digits)
                 sell_condition = get_sell_condition(df_signal, now_datetime, digits)
-
+                
+                
                 if buy_condition == True:
                     print("there are buy signal at {} \n".format(now_datetime))
                 elif sell_condition == True:
                     print("there are sell signal at {} \n".format(now_datetime))
                 else:
                     print("no signal yet, be patience! \n")
+                """
+
             elif len(order_amount) != 0 and trade_check == False:
                 trade_check = True
                 print("there is an order, no trade on {} just yet \n".format(symbol))       
